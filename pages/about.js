4a786f82852f84b35d7b62922004ead74dee218d
Purna Mahesh Immandi <poorna.sd9@gmail.com>
2021-08-03 15:41:49 +0530

    Layout, Header and Fotter

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+function about() {
+    return (
+        <Layout title="About DJ Events">
+            <h1>About Page</h1>
+        </Layout >
+    )
+}
+
+export default about
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout"
+
+export default function add() {
+    return (
+        <Layout title='Add Event'>
+            <h1>Add Events</h1>
+        </Layout>
+    )
+}
